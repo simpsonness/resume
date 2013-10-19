24786cc0ee5249ff8ac331b5b50d49cb3b06d2aa
shan <corallishan@gmail.com>
2013-10-19 05:51:48 -0400

    the second commit change for assignments.rb

--- assignments.rb
+++ assignments.rb
@@ -6,20 +6,22 @@ end
 
 
 
-def favorite_cities(array)
-  array=["ChengDu", "Como", "Doha", "HongKong", "NewYork"]
-  array.each do |city|
+
+ favorite_cities=["ChengDu", "Como", "Doha", "HongKong", "NewYork"]
+  favorite_cities.each do |city|
     puts city
   end
-end
+
   
 
  for num in 1..100 do 
-    if num%3==0
-      print "Bitmaker"
+    if num%3==0&&num%5==0
+      print "BitmakerLabs, "
+    elsif num%3==0
+      print "Bitmaker, "
     elsif num%5==0
-      print "Labs"
-    else num%3==0 && num%5==0
-      print "BitmakerLabs"
+      print "Labs, "
+    else
+      print "#{num}, "
     end
   end
